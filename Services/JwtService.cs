4edf64b11984b7d50907bf04bde28536a41ceaba
Hasintha <aashi.chandu5088@gmail.com>
2025-03-20 07:58:08 +0530

    complete code after the token generate

--- Services/JwtService.cs
+++ Services/JwtService.cs
@@ -7,40 +7,6 @@ using Backend.Models;
 
 namespace Backend.Services
 {
-     // public class JwtService
-     // {
-     //      private readonly IConfiguration _configuration;
-
-     //      public JwtService(IConfiguration configuration)
-     //      {
-     //           _configuration = configuration;
-     //      }
-
-     //      public string GenerateToken(UserModel user)
-     //      {
-     //           var tokenHandler = new JwtSecurityTokenHandler();
-     //           Console.WriteLine($"tokenHandler: {tokenHandler}");
-
-     //           var key = Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]);
-     //           Console.WriteLine($"key: {key}");
-
-     //           var tokenDescriptor = new SecurityTokenDescriptor
-     //           {
-     //                Subject = new ClaimsIdentity(new[]
-     //               {
-     //                new Claim(ClaimTypes.NameIdentifier, user.UserId.ToString()),
-     //                new Claim(ClaimTypes.Email, user.email)
-     //            }),
-     //                Expires = DateTime.UtcNow.AddHours(1),
-     //                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
-     //           };
-     //           Console.WriteLine($"tokenDescriptor: {tokenDescriptor}");
-     //           var token = tokenHandler.CreateToken(tokenDescriptor);
-     //           Console.WriteLine($"token: {token}");
-     //           return tokenHandler.WriteToken(token);
-     //      }
-     // }
-
      public class JwtService
      {
           private readonly string _secretKey;
--- obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b5956fe3bcb422f45cdfd20e16d74e9c91993ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22e7a3196797ba7ab9bd53e60ea9cf350acc9744")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
